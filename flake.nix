fb7e145a1b9efbddd4e550b6aa41624e0f3ed1fe
Karitham <kar@karitham.dev>
2024-01-08 10:39:53 +0100

    flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils/main";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      rec {
+        devShell = pkgs.mkShell {
+          name = "corde";
+          packages = with pkgs; [
+            go_1_21
+          ];
+        };
+      });
+}
